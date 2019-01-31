6612112498ed50a551796926f6d8568a946542ca
Waga Odongo <wagaodongo@gmail.com>
2019-01-31 11:26:25 +0300

    good
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -5,9 +5,16 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 import BMC from '../images/BMC.jpg';
+import styled from "styled-components";
+
+const HeaderWrapper =styled.div`
+    background:  #524763;
+    margin-bottom: 1.45rem; 
+`;
 
 const IndexPage = () => (
-  <Layout>
+  <HeaderWrapper>
+    <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -20,6 +27,9 @@ const IndexPage = () => (
     <Link to="/about">About</Link>
 
   </Layout>
-)
+
+
+  </HeaderWrapper>
+  )
 
 export default IndexPage
