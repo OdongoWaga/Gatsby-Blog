81203bb9dda6d691d878696a151ac2b3d2078bfc
Waga Odongo <wagaodongo@gmail.com>
2019-01-31 10:32:33 +0300

    setting up index
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ blog/src/pages/about.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Layout from '../components/layout';
+
+export default class About extends Component {
+  render() {
+    return (
+
+        <Layout>
+            <div>
+        <h1>About Us </h1>
+        <p> A fairly successful career in sports I did a lot of 
+        modeling work in the mid-80s I'm really good at that means I am wonderful. I'm too honest beekeeping I'm an enormous man-child staying up late snapchat, complete lack of shame on the first date that's what she said lol crossfit. When I get drunk I grow a creepy mustache every February see, I told you I am extremely experienced and talented I should have grown up in the 40s performance art. 
+        </p>
+      </div>
+
+        </Layout>
+      
+    )
+  }
+}
