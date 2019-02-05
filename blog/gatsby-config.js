3d1b9b02c0281c476871c52acfb56f2dfaef9d3e
Waga Odongo <wagaodongo@gmail.com>
2019-02-05 15:17:12 +0300

    init
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `All You Need Is Love`,
-    author: `Waga`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,35 +1,21 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
-import BMC from '../images/BMC.jpg';
-import styled from "styled-components";
-
-const HeaderWrapper =styled.div`
-    background:  #524763;
-    margin-bottom: 1.45rem; 
-`;
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <HeaderWrapper>
-    <Layout>
+  <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-
-      <img src={BMC} alt="BMC" />
     </div>
-    <Link to="/about">About</Link>
-
+    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-
-
-  </HeaderWrapper>
-  )
+)
 
 export default IndexPage
